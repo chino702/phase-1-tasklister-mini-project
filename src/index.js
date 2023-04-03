7723cd3bc7e7d93105bdf15f1eacc52d78d45dd3
chino702 <chinox3702@gmail.com>
2023-04-02 23:06:40 -0700

    task lister mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector('#create-task-form');
+  const tasks = document.querySelector('#tasks');
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission
+
+    const input = document.querySelector('#new-task-description');
+    const task = document.createElement('li');
+    task.innerText = input.value;
+    tasks.appendChild(task);
+
+    input.value = ''; // Clear input field
+  });
 });
